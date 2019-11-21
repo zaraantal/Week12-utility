d49387839dc659788baab96005fd79ac1d4664cb
zaraantal <57725987+zaraantal@users.noreply.github.com>
2019-11-21 10:31:03 -0700

    function 2

---
+++ Week12-utility2.py
@@ -0,0 +1,14 @@
+#Zara Antal
+#CSCI 102 - Section C
+#Week 12 - Part A
+
+def PrintOutput(word):
+    PrintOutput(word)
+
+def LoadFile(file):
+    list1 = []
+    my_file = open(filename)
+    for line in my_file:
+        list1.append(line.strip())
+    my_file.close()
+    return list1
