cfd1702353dd9924c49580c971c086dee689eb33
zaraantal <57725987+zaraantal@users.noreply.github.com>
2019-11-21 10:34:42 -0700

    function 5

--- Week12-utility2.py
+++ Week12-utility2.py
@@ -24,3 +24,15 @@ def FindWordCount(list1, string):
         if string in i:
             count = count + 1
     return count
+
+def ScoreFinder(player_names, player_scores, player_to_find):
+    scores = -1
+    for i in range(len(player_names)):
+        if(player_names[i].lower() == player_to_find.lower()):
+            scores = player_scores[i]
+            break
+    if(player_scores == -1):
+        print("player not found")
+    else:
+        print(player_to_find,"got a score of",score)
+
